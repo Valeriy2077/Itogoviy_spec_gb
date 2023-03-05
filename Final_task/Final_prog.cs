d9c50d90ad816afd539b87c3611540c5b322c9a7
Valeriy2077 <84186810+Valeriy2077@users.noreply.github.com>
2023-03-06 01:51:51 +0300

    Теперь я добавил блок-схему

--- Final_task/Final_prog.cs
+++ Final_task/Final_prog.cs
@@ -1,1 +1,56 @@
 ﻿Console.Clear();
+string[] array = new string[] { "Hello", "13", "world", "=)" };
+string[] arrayN = new string[array.Length];
+
+void NewArray()
+{
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            arrayN[j] = array[i];
+            j++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+NewArray();
+PrintArray(arrayN);
+
+
+
+// string[] array = new string[] { "Hello", "2", "world", ":-)" };
+// string[] arrayNew = new string[array.Length];
+
+// void NewArray()
+// {
+//     int j = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i].Length <= 3)
+//         {
+//             arrayNew[j] = array[i];
+//             j++;
+//         }
+//     }
+// }
+
+// void PrintArray(string[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write($"{array[i]} ");
+//     }
+//     Console.WriteLine();
+// }
+// NewArray();
+// PrintArray(arrayNew);
